786bf987a9524ebdd542eb658b1cb29f6a1578e3
Yaroslav <asdfc1234987@gmail.com>
2025-11-08 18:42:11 +0200

    Laba2

---
+++ Laba2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+
+        int babyka = 0;
+        foreach (int c in input )
+        {
+            if (c == 'I' )
+                babyka++;
+            else if (c  "IV" )
+                babyka+=4;
+            else if (c == 'V')
+                babyka+=10;
+            else if (c == 'X' )
+                babyka+=5;
+        }
+
+        Console.WriteLine($"Сумма = {babyka}");
+
+
+    }
+}
